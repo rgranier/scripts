528dc0bde2edbf30bbc72a890d9e95870fbfc6e9
Randall Granier <rgranier@gmail.com>
2021-03-25 14:24:24 -0400

    Build a server block

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# scripts
+# Script Collection
+
+## General Instructions
+This repo needs to be cloned on all servers.
+
+## repo
+https://github.com/rgranier/scripts.git
+
+## Auth and Cheats
+Ensure git is installed, which it should be for Ubuntu 18.04
+When this is cloned, it makes the scripts directory.
+Do this under the ubuntu user, not root
+
+NOTE:  Ubuntu 18 is not set up by default with the nice git profile that
+shows the branch in the path.
+
+git clone https://github.com/rgranier/scripts.git
---
+++ linux/build-nginx.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+WEB='bluesandtechnology.com'
+USER='www-data'
+DIR="/var/www/$WEB/html"
+
+if [ -d "$DIR"]; then
+    echo "ERROR: This has already been run and needs cleanup."
+    exit 1
+fi
+
+sudo mkdir -p $DIR
+sudo chown -R $USER:$USER /var/www/$WEB/html
+
+sudo chmod -R 755 /var/www/$powderblue
+
+sudo echo "<html>
+    <head>
+        <title>Welcome to $WEB!</title>
+    </head>
+    <body>
+        <h1>Success!  The example.com server block is working!</h1>
+    </body>
+</html>" >
+
+
+sudo echo "server {
+        listen 80;
+        listen [::]:80;
+
+        root /var/www/$WEB/html;
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name $WEB www.$WEB;
+
+        location / {
+                try_files $uri $uri/ =404;
+        }
+}" > /etc/nginx/sites-available/$WEB
+
+
+sudo ln -s /etc/nginx/sites-available/$WEB /etc/nginx/sites-enabled/
